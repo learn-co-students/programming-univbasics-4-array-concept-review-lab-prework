e6ceb32f237f8c702fe233925d27ac6e2ab5d3ca
github username <github email address>
2019-10-19 21:32:25 -0400

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,23 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+       array.length.times {|index| 
+      if array[index] == value_to_find
+        return index
+      end 
+      }
+      nil
+    end
 
-def find_max_value(array)
-  # Add your solution here
-end
+ def find_max_value(array)
+    array.length.times { |highest_value|
+    return array.max
+    }
+end 
+
+
 
 def find_min_value(array)
-  # Add your solution here
+    array.length.times { |lowest_value|
+    return array.min
+    }
+ 
 end
