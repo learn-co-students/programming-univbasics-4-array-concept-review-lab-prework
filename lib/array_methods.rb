a4595bd838853041594ac3b687c69ad482c99905
Bleak Chandler <bleakchandler@gmail.com>
2021-01-19 22:31:23 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,20 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  newarray = array
+  value = value_to_find
+
+  p newarray.find_index(value)
+ 
 end
 
 def find_max_value(array)
-  # Add your solution here
+  newarray = array
+
+  p newarray.max
+
 end
 
 def find_min_value(array)
-  # Add your solution here
+  newarray = array
+
+  p newarray.min
 end
